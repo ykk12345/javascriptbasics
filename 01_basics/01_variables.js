29ffb508cc8139f0ea48f46702e05eb8d0938771
Yash kumar kaushik <108153942+ykk12345@users.noreply.github.com>
2024-02-06 15:59:02 +0000

    pending

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=144553
+let accountEmail="kaushikyash533@gmail.com"
+var accoutPassword="12345"
+accountCity="Faridabad"
+
+// accountId = 2  // not allowed
+
+accountEmail="kaushiky660@gmail.com"
+accountPassword = "3234"
+accountCity="Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
