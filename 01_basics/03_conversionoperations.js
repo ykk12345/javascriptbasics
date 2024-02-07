5bcacf45d10b4298561f8687ff4e782cb7356b8f
Yash kumar kaushik <108153942+ykk12345@users.noreply.github.com>
2024-02-07 18:19:23 +0000

    conversions of string to number , number to boolean , etc

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,24 @@
+let score = "33absc"
+//console.log(typeof score)
+
+// typeconversion
+
+let valueinnumber = Number(score)
+//console.log(typeof(valueinnumber))
+
+// "33" -> 33
+// "33abc" -> NaN -> not a number
+//true -> 1, false ->0
+//null - > 0
+//undefiend - > NaN
+
+let isloggedin = ""
+
+let booleanisloggedin = Boolean(isloggedin)
+
+console.log(booleanisloggedin);
+
+// 1 -> true
+//0 ->false
+// "" ->false
+//"yash" -> true
