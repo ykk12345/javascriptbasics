1cc9e711c2839e8d2590e34b8e17b21c349f33db
Yash kumar kaushik <108153942+ykk12345@users.noreply.github.com>
2024-02-07 18:40:20 +0000

    operations

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -16,9 +16,31 @@ let isloggedin = ""
 
 let booleanisloggedin = Boolean(isloggedin)
 
-console.log(booleanisloggedin);
+// console.log(booleanisloggedin);
 
 // 1 -> true
 //0 ->false
 // "" ->false
 //"yash" -> true
+
+// ******************operations********************
+
+   let value = 3
+   let negvalue = -value
+
+//    console.log(2+2)
+//    console.log(2-2)
+//    console.log(2*2)
+//    console.log(2**2)
+//    console.log(2/2)
+//    console.log(2%2)
+
+   let str1 = "yash"
+   let str2=" kaushik"
+   let str3 = str1+str2
+   console.log(str3)
+
+   console.log("1"+2);
+   console.log(1+"2");
+   console.log("1"+2+2);
+   console.log(1+2+"2");
