62edf9bf7d84ca05606b98a066d9a17c9403f551
Yash kumar kaushik <108153942+ykk12345@users.noreply.github.com>
2024-02-09 16:37:35 +0000

    Math and Number

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,28 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(typeof balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const  othernumber= 123.899;
+// console.log(othernumber.toPrecision(4));
+
+// ++++++++++++++++++ Maths ++++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.random()); // gives a random value between 0 to 1 
+// console.log((Math.random()*10)+1); // min value 1 aayegi
+
+const min =10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * ( max- min + 1) +min ));
